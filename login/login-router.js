b07a3243d919c5828aa02d455f6843af11ac48f2
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-06-15 16:35:44 -0400

    Created register function

--- login/login-router.js
+++ login/login-router.js
@@ -11,4 +11,20 @@ router.get("/users", (req, res) => {
     })
 })
 
+router.post("/register", (req, res) => {
+
+    const {username, password} = req.body;
+    const rounds = process.env.HASH_ROUNDS || 8;
+    const hash = bcrypt.hashSync(password, rounds);
+
+    Users.add({username, password: hash})
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.status(500).json(err.message)
+        console.log(err)
+    })
+})
+
 module.exports = router;
